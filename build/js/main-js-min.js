b33997447f7092ef1b274de8a220a1edacba520c
MishaChernov <80amulet63@gmail.com>
2017-09-30 22:08:38 +0300

    Первая страница: Ответы на вопросы

---
+++ build/js/main-js-min.js
@@ -0,0 +1,1 @@
+"use strict";$(".no-js").removeClass("no-js"),$(".burger").click(function(){$(this).toggleClass("burger--close burger--open"),$(".main-menu__list").slideToggle()});
---
+++ build/js/main-js.js
@@ -0,0 +1,8 @@
+'use strict'
+
+$('.no-js').removeClass('no-js');
+
+$('.burger').click(function(){
+  $(this).toggleClass('burger--close burger--open');
+  $('.main-menu__list').slideToggle();
+});
