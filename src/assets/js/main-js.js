03e2bddfc9c5dfccecee4e047555048ce834f3bd
MishaChernov <80amulet63@gmail.com>
2017-09-30 22:10:12 +0300

    Update 2017-09-30T19:10:05.455Z

--- src/assets/js/main-js.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-$('.no-js').removeClass('no-js');
-
-$('.burger').click(function(){
-  $(this).toggleClass('burger--close burger--open');
-  $('.main-menu__list').slideToggle();
-});
